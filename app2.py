fb7faa6c0137668a9c86bc62bf4ebfa682a7daf4
hyeseon99 <hyeseon0416@gmail.com>
2024-09-12 15:45:22 +0900

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,42 +1,43 @@
+import plotly.express as px
+import plotly
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-money = pd.read_csv("money_data7.csv")
+# 한글 안깨지게 하는 코드 
+from matplotlib import font_manager, rc
+font = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font)
 
-option = st.selectbox(
-    'How would you like to choice year ?',
-    ('2020', '2021', '2022'))
+#plt.figure(figsize=(12,8))
 
-option2 = int(option)
+money = pd.read_csv("c:\\data\\money_data7.csv")
 
-st.write('You selected:', option)
-
-money = money[:] [money['A_YEAR']== option2]
+money = money[:] [money['A_YEAR']==2022]
 
 fig, ax = plt.subplots(2,2, figsize=(12,8))
 
 plt.subplot(221)
 plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('America rate')
+plt.title('미국금리')
 
 
 plt.subplot(222)
 plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('Korea rate')
+plt.title('한국금리')
 
 plt.subplot(223)
 plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('Kospi Rate')
+plt.title('코스피 지수')
 
 plt.subplot(224)
 plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
 plt.xticks(tuple(money['A_MONTH']) )
-plt.title('House Price')
+plt.title('집값')
 
 st.pyplot(fig)
